72df0364707ab7131a7dbce17f221da3b894deb1
David <david@david-VPCEB26GM.(none)>
2013-12-28 23:32:48 -0500

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,13 +6,17 @@ describe User do
                      password: "foobar", password_confirmation: "foobar")
   }
   subject { @user }
-
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
   it { should be_valid }
 
   describe "when name is not present" do
